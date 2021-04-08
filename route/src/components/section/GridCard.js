1c32dfa355db3e6160a5f40de90b2e64b0c5842b
Ganesh-dot <gsk1664@gmail.com>
2021-04-08 12:57:15 +0530

    push grud layout and style

--- route/src/components/section/GridCard.js
+++ route/src/components/section/GridCard.js
@@ -1,13 +1,52 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import "./GridCard.css";
 
-class GridCard extends Component {
-  render() {
-    return (
-      <div>
-        <h2>GridCard Component</h2>
+const GridCard = () => {
+  const getUsers = async () => {
+    const response = await fetch("https://api.github.com/users");
+    const data = await response.json();
+  };
+  useEffect(() => {
+    getUsers();
+  });
+  return (
+    <>
+      <h2>List of Users</h2>
+      <div className="container-fluid mt-5">
+        <div className="row text-center">
+          <div className="col-10 col-md mt-5">
+            <div className="card p-2">
+              <div className="d-flex align-items-center">
+                <div className="image">
+                  <img src="" className="rounded" width="155"></img>
+                </div>
+                <div className="ml-3 w-100">
+                  <h4 className="mb-0 mt-0 textleft">
+                    GridCard
+                    <span className="textleft">Web Developers</span>
+                  </h4>
+                  <div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
+                    <div className="d-flex flex-collumn">
+                      <span className="articles">Articles</span>
+                      <span className="number1">38</span>
+                    </div>
+                    <div className="d-flex flex-collumn">
+                      <span className="articles">Followers</span>
+                      <span className="number1">980</span>
+                    </div>
+                    <div className="d-flex flex-collumn">
+                      <span className="articles">Rating</span>
+                      <span className="number1">8.9</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  }
-}
+    </>
+  );
+};
 
 export default GridCard;
--- route/src/components/section/Table.js
+++ route/src/components/section/Table.js
@@ -1,47 +1,22 @@
-import React from "react";
-import { useTable } from "react-table";
-import "./Table.css";
-import Mock_JSON_Data from "./Mock_JSON_Data";
-import { Columns } from "./Columns";
+import React, { Component } from "react";
 
-const Table = () => {
-  const columns = useMemo(() => COLLUMNS, []);
-  const data = useMemo(() => MOCK_DATA, []);
-  const tableInstance = useTable({
-    columns,
-    data,
-  });
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRows,
-  } = tableInstance;
-  return;
-  <table {...getTableProps()}>
-    <thead>
-      {headerGroups.map((headerGroups) => (
-        <tr {...headerGroups.getHeaderGroupProps()}>
-          {headerGroups.headers.map((column) => (
-            <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-          ))}
-          <th></th>
-        </tr>
-      ))}
-    </thead>
-    <tbody {...getTableBodyProps()}>
-      {rows.map((row) => {
-        prepareRows(row);
-        return (
-          <tr {...row.getRowProps()}>
-            {row.cells.map((cell) => {
-              return <td {...cell.getCellProps()}>cell.render("Cell")</td>;
-            })}
-          </tr>
-        );
-      })}
-    </tbody>
-  </table>;
-};
+class Table extends Component {
+  state = {
+    loading: false,
+    person: null,
+  };
+
+  async componentDidMount() {
+    const url = "https://api.randomuser.me/";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({
+      person: data.results[0],
+    });
+  }
+  render() {
+    return <div></div>;
+  }
+}
+
 export default Table;
