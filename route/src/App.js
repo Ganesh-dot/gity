9131c8706b4503a162ed3ef197a79547c54663c4
Ganesh-dot <gsk1664@gmail.com>
2021-04-08 12:58:21 +0530

    push src folder

--- route/src/App.js
+++ route/src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
+import "./App.css";
 import Header from "./components/Header";
 import Section from "./components/Section";
+import Table from "./components/section/Table";
+import GridCard from "./components/section/GridCard";
 import { BrowserRouter as Router } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <div>
-          <Header />
-          <Section />
-        </div>
+        <Header />
+        <Section />
+        <GridCard />
+        <Table />
       </Router>
     );
   }
