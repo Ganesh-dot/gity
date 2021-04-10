75667bdfe859b2dd8e49c894282da57e47df9276
Ganesh-dot <gsk1664@gmail.com>
2021-04-10 12:43:24 +0530

    push root file and root css and index css file

--- route/src/App.js
+++ route/src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
-import "./App.css";
 import Header from "./components/Header";
 import Section from "./components/Section";
 import Table from "./components/section/Table";
-import GridCard from "./components/section/GridCard";
 import { BrowserRouter as Router } from "react-router-dom";
 
 class App extends React.Component {
@@ -11,9 +9,8 @@ class App extends React.Component {
     return (
       <Router>
         <Header />
-        <Section />
-        <GridCard />
         <Table />
+        <Section />
       </Router>
     );
   }
